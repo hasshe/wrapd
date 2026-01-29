9886e37344b486055205f084e1b1db059a6a98c5
hasshe <hassan.sheikha_97@outlook.com>
2026-01-29 18:49:06 +0100

    Initial Commit

---
+++ wrapd/views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  wrapd
+//
+//  Created by Hassan Sheikha on 2026-01-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Button("Hello, world!") {
+                print("Hello, world!ddddss")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wrapd/wrapdApp.swift
@@ -0,0 +1,17 @@
+//
+//  wrapdApp.swift
+//  wrapd
+//
+//  Created by Hassan Sheikha on 2026-01-29.
+//
+
+import SwiftUI
+
+@main
+struct wrapdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
